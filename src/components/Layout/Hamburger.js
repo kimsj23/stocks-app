3eb5d3e6295793a3292e3e5f8ddc61dcbf0cda4f
Jason Kim <jkim0288@gmail.com>
2022-05-09 18:19:06 -0700

    Add responsive hamburger icon

---
+++ src/components/Layout/Hamburger.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hamburger.css";
+
+function Hamburger() {
+  return (
+    <>
+      <div className="burger-click">
+        <div className="hamburger" />
+        <div className="hamburger" />
+        <div className="hamburger" />
+      </div>
+    </>
+  );
+}
+
+export default Hamburger;
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import Hamburger from "./Hamburger";
 
 function Navbar() {
   return (
@@ -24,6 +25,7 @@ function Navbar() {
             </Link>
           </li>
         </ul>
+        <Hamburger />
       </nav>
     </header>
   );
