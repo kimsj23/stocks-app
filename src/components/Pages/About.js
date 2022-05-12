251d84b9c5c05d1e7afe3b88c9954ab652fafa62
Jason Kim <jkim0288@gmail.com>
2022-05-12 15:11:12 -0700

    Fix text positioning

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -4,7 +4,7 @@ import "./About.css";
 function About() {
   return (
     <div className="about">
-      <h1>Welcome to StocksMomo!</h1>
+      <h1 className="about-title">Welcome to StocksMomo!</h1>
       <p>
         This site was created to help traders and investors to see what specific
         stocks are trending as of this moment. We give the tools to make
